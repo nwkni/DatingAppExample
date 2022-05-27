36b787a900a4f128fc298dc2512b40ac62a7fc00
Nathaniel <nwhidle@gmail.com>
2022-05-27 10:38:53 -0700

    Services updates + UI Start & basic function

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
-  ngOnInit(){
-   this.getUsers() ;
+  constructor( private accountService: AccountService) { }
+  
+  ngOnInit() {
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      {
-        next: (result:any) => {this.users = result},
-  
-        error: (error:any) => {console.log(error)}
-        
-      })
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'))
+    this.accountService.setCurrentUser(user)
   }
+
+
 }
