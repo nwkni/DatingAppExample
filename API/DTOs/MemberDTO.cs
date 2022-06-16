ad14a74d946c9648f9f85fae290275291a3c6990
Nathaniel <nwhidle@gmail.com>
2022-06-16 16:21:21 -0700

    Database changes + Seeding + Data Querying

---
+++ API/DTOs/MemberDTO.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+
+namespace API.DTOs
+{
+    public class MemberDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age {get; set;}
+        public string Nickname { get; set; }
+        public DateTime Created { get; set; } 
+        public DateTime LastActive { get; set; } 
+        public String Gender { get; set; }
+        public String Introduction { get; set; }
+        public String LookingFor { get; set; }
+        public String Interests { get; set; }
+        public String City { get; set; }
+        public String Country { get; set; }
+        public ICollection<PhotoDTO> Photos { get; set; }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,17 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
-        
         public string UserName { get; set; }
-
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth {get; set;}
+        public string Nickname { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public String Gender { get; set; }
+        public String Introduction { get; set; }
+        public String LookingFor { get; set; }
+        public String Interests { get; set; }
+        public String City { get; set; }
+        public String Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        // public int getAge() {
+        //     return DateOfBirth.CalculateAge() ;
+        // }
     }
 }
